bbf64235079b8442deba993fea0fcb4c424860f3
amauriciorr <robayo.m.antonio@gmail.com>
2019-12-24 22:20:18 -0500

    added TO-DO notes for next steps

--- build_rapbot.py
+++ build_rapbot.py
@@ -2,8 +2,12 @@ from args import *
 from training_utils import *
 from processing_utils import * 
 
-
 if __name__ == "__main__":
     preprocess_args = get_setup_args()
     get_and_process_songs(preprocess_args)
-
+        '''
+        TO ADD
+        1) FUNCTION FOR CREATING ChatDataset() + ChatDataset() CLASS OBJECTS AND SAVING THEM OR FEEDING THEM FOR USE IN TRAINING CLASS/WORKFLOW
+        2) ADD ABOVE AS PRE-SAVED FILES FOR SKIPPING DOWNLOAD/PREPROCESS SET???
+        3) TRAINING MODEL PORTION
+        '''
