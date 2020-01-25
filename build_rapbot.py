524ddba4cb63418a75372da5af534a35355460db
amauriciorr <robayo.m.antonio@gmail.com>
2020-01-25 16:01:38 -0500

    remove setup from building/ training rapbot

--- build_rapbot.py
+++ build_rapbot.py
@@ -1,15 +1,15 @@
+import os
 import pickle as pkl
-from args import * 
-from training_utils import *
+from args import get_train_args 
 from processing_utils import * 
 
 if __name__ == "__main__":
-    preprocess_args = get_setup_args()
-    get_and_process_songs(preprocess_args)
     chat_dict = ChatDictionary('./word_counts_dict.p')
     train_dataset = ChatDataset(chat_dict,'./train_lyrics.jsonl')
     valid_dataset = ChatDataset(chat_dict,'./valid_lyrics.jsonl', 'valid')
     training_args = get_train_args()
+    if not os.path.exists(training_args.save_dir):
+        os.makedirs(training_args.save_dir)
     train_dataloader = build_dataloader(train_dataset, batchify, training_args.batch_size)
     valid_dataloader = build_dataloader(valid_dataset, batchify, training_args.batch_size)
 
