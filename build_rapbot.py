d7762f0cb51103e6ad0d4b73e3dc6e116a3958fc
amauriciorr <robayo.m.antonio@gmail.com>
2020-07-11 22:49:10 -0400

    refactor spaces to tabs, correct mismatched import statements

--- build_rapbot.py
+++ build_rapbot.py
@@ -1,30 +1,31 @@
 import os
 import pickle as pkl
-from args import get_train_args 
-from processing_utils import * 
+from args import get_train_args
+from training_utils import * 
+
 
 if __name__ == "__main__":
-    chat_dict = ChatDictionary('./word_counts_dict.p')
-    train_dataset = ChatDataset(chat_dict,'./train_lyrics.jsonl')
-    valid_dataset = ChatDataset(chat_dict,'./valid_lyrics.jsonl', 'valid')
-    training_args = get_train_args()
-    if not os.path.exists(training_args.save_dir):
-        os.makedirs(training_args.save_dir)
-    train_dataloader = build_dataloader(train_dataset, batchify, training_args.batch_size)
-    valid_dataloader = build_dataloader(valid_dataset, batchify, training_args.batch_size)
+	chat_dict = ChatDictionary('./word_counts_dict.p')
+	train_dataset = ChatDataset(chat_dict,'./train_lyrics.jsonl')
+	valid_dataset = ChatDataset(chat_dict,'./valid_lyrics.jsonl', 'valid')
+	training_args = get_train_args()
+	if not os.path.exists(training_args.save_dir):
+		os.makedirs(training_args.save_dir)
+	train_dataloader = build_dataloader(train_dataset, batchify, training_args.batch_size)
+	valid_dataloader = build_dataloader(valid_dataset, batchify, training_args.batch_size)
 
 
-        '''
-        TO ADD
-        1) function that wraps entire training process into one line, i.e. feed training args to function
- 		2) model architecture (standard + BERT)
- 			encoder
- 			decoder
- 			attn mechanism
- 		3) default model config as part of args.py
-        4) TRAINING MODEL PORTION/ training loop class
+		'''
+		TO ADD
+		1) function that wraps entire training process into one line, i.e. feed training args to function
+		2) model architecture (standard + BERT)
+			encoder
+			decoder
+			attn mechanism
+		3) default model config as part of args.py
+		4) TRAINING MODEL PORTION/ training loop class
 
-        TO TEST:
-        1) ADDING BERT
-        2) LSTM VS GRU 
-        '''
+		TO TEST:
+		1) ADDING BERT
+		2) LSTM VS GRU 
+		'''
--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
 import pickle as pkl
 from args import get_setup_args
-from training_utils import *
+from processing_utils import * 
 
 
 def get_and_process_songs(args):
-    stock_filename = 'Lyrics_' + args.artist_name.replace(' ', '') + '.json'
-    stock_filename = sanitize_filename(stock_filename)
+	stock_filename = 'Lyrics_' + args.artist_name.replace(' ', '') + '.json'
+	stock_filename = sanitize_filename(stock_filename)
 
-    if args.download_lyrics:
-        start = dt.datetime.now()
-        print('{}| Beginning download'.format(start))
-        genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN, sleep_time=1)
-        artist_tracks = force_search_artist(genius, args.artist_name, sleep_time=600, max_songs=None,
-                                             sort='popularity', per_page=20,
-                                             get_full_info=True,
-                                             allow_name_change=True)
-        print('{}| Finished download in {}'.format(dt.datetime.now(), (dt.datetime.now() - start)))
-        artist_tracks.save_lyrics()
+	if args.download_lyrics:
+		start = dt.datetime.now()
+		print('{}| Beginning download'.format(start))
+		genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN, sleep_time=1)
+		artist_tracks = force_search_artist(genius, args.artist_name, sleep_time=600, max_songs=None,
+											 sort='popularity', per_page=20,
+											 get_full_info=True,
+											 allow_name_change=True)
+		print('{}| Finished download in {}'.format(dt.datetime.now(), (dt.datetime.now() - start)))
+		artist_tracks.save_lyrics()
+	elif args.load_path:
+	  
 
-    genius_file = read_json('./'+stock_filename)    
-    word_counts = create_counts_dict(genius_file, RETOK)
-    pkl.dump(word_counts, open('word_counts_dict.p','wb'))
-    artist_lyrics = get_lyrics_from_json(genius_file, SONG_PART_REGEX)
-    create_text_and_target(artist_lyrics, len(artist_lyrics))
+	genius_file = read_json('./'+stock_filename)    
+	word_counts = create_counts_dict(genius_file, RETOK)
+	pkl.dump(word_counts, open('word_counts_dict.p','wb'))
+	artist_lyrics = get_lyrics_from_json(genius_file, SONG_PART_REGEX)
+	create_text_and_target(artist_lyrics, len(artist_lyrics))
 
 
 if __name__ == "__main__":
