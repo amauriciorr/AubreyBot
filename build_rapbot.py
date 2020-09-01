30bd81bb3393c28750198c00c0e9e55e96b5e3db
amauriciorr <robayo.m.antonio@gmail.com>
2020-08-31 23:44:58 -0400

    finish training initialization

--- build_rapbot.py
+++ build_rapbot.py
@@ -1,31 +1,33 @@
 import os
 import pickle as pkl
 from args import get_train_args
-from training_utils import * 
+from training_utils import *
 
 
 if __name__ == "__main__":
-	chat_dict = ChatDictionary('./word_counts_dict.p')
-	train_dataset = ChatDataset(chat_dict,'./train_lyrics.jsonl')
-	valid_dataset = ChatDataset(chat_dict,'./valid_lyrics.jsonl', 'valid')
-	training_args = get_train_args()
-	if not os.path.exists(training_args.save_dir):
-		os.makedirs(training_args.save_dir)
-	train_dataloader = build_dataloader(train_dataset, batchify, training_args.batch_size)
-	valid_dataloader = build_dataloader(valid_dataset, batchify, training_args.batch_size)
+    chat_dict = ChatDictionary('./word_counts_dict.p')
+    train_dataset = ChatDataset(chat_dict, './train_lyrics.jsonl')
+    valid_dataset = ChatDataset(chat_dict, './valid_lyrics.jsonl', 'valid')
+    training_args = get_train_args()
 
+    if not os.path.exists(training_args.save_dir):
+        os.makedirs(training_args.save_dir)
 
-		'''
-		TO ADD
-		1) function that wraps entire training process into one line, i.e. feed training args to function
-		2) model architecture (standard + BERT)
-			encoder
-			decoder
-			attn mechanism
-		3) default model config as part of args.py
-		4) TRAINING MODEL PORTION/ training loop class
+    train_dataloader = build_dataloader(train_dataset, batchify, training_args.batch_size)
+    valid_dataloader = build_dataloader(valid_dataset, batchify, training_args.batch_size,
+                                        shuffle=False)
 
-		TO TEST:
-		1) ADDING BERT
-		2) LSTM VS GRU 
-		'''
+    opts = set_model_config(chat_dict, training_args.hidden_size, training_args.embedding_size,
+                            training_args.num_layers_enc, training_args.num_layers_dec,
+                            training_args.dropout)
+
+    model = seq2seq(opts)
+    model.to(current_device)
+    criterion = nn.CrossEntropyLoss(ignore_index=0, reduction='sum')
+    optimizer = torch.optim.Adam(chat_model.parameters(), 0.01, amsgrad=True)
+
+    model_trainer = seq2seqTrainer(seq2seq, train_dataloader, valid_dataloader, criterion,
+                                   optimizer, training_args.learning_rate, training_args.with_cuda,
+                                   training_args.num_epochs, training_args.save_dir)
+
+    model_trainer.train_model()
