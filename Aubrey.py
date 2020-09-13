0781820b1a06406450294096c64c6430dda1d18c
amauriciorr <robayo.m.antonio@gmail.com>
2020-09-13 18:23:06 -0400

    update to allow for custom chatbot dict

--- Aubrey.py
+++ Aubrey.py
@@ -14,7 +14,7 @@ from args import get_chat_args
 if __name__ == '__main__':
     chatbot_args = get_chat_args()
     current_device = torch.device("cuda" if chatbot_args.with_cuda else "cpu")
-    chat_dict = ChatDictionary('./word_counts_dict.p')
+    chat_dict = ChatDictionary(chatbot_args.vocab_path)
     opts = set_model_config(chat_dict)
     model_checkpoint = torch.load(chatbot_args.model_path, map_location = current_device)
     chatbot = seq2seq(opts)
