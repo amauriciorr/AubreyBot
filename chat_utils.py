071daa8ed1aaf0222d90efab66d8ef489d40b046
amauriciorr <robayo.m.antonio@gmail.com>
2020-12-26 00:54:47 -0500

    replace CLI args with randomly generated params

--- chat_utils.py
+++ chat_utils.py
@@ -163,19 +163,21 @@ def start_rapbot(model, chat_dictionary, p, device, transformer = False):
         user_batch = mini_batchify(context, chat_dictionary, device)
     return context
 
-def transfer_learning_bot(model, tokenizer, max_length, top_k, top_p, repetition_penalty, no_repeat_ngram_size, length_penalty):
+def transfer_learning_bot(model, tokenizer, max_length, top_k, top_p):
     input_sentence = input('User >> ')
     input_sentence = input_sentence.lower()
     context = copy(input_sentence)
     input_sentence = tokenizer.encode(input_sentence, truncation = True, max_length = 128, return_tensors = 'pt')
     continue_convo = True
     while continue_convo:
-        uni_temp = round(torch.rand(1).clamp(0.1).item(), 2)
         print(bash_format_text('Typing...', 'YELLOW', 'BOLD'), end='\r' )
+        uni_temp = round(torch.rand(1).clamp(0.1).item(), 2)
+        repeat_penalty = round((torch.rand(1) * 5).clamp(1).item(), 2)
+        ngram =  int(np.random.choice([2,3,4], 1)[0])
         bot_reply = model.generate(input_sentence, max_length = max_length, top_k = top_k, top_p = top_p, temperature = uni_temp, 
-                                   repetition_penalty = repetition_penalty, skip_special_tokens = True,
-                                   no_repeat_ngram_size=no_repeat_ngram_size, pad_token_id = tokenizer.eos_token_id,
-                                   length_penalty=length_penalty)
+                                   repetition_penalty = repeat_penalty, skip_special_tokens = True,
+                                   no_repeat_ngram_size=ngram, pad_token_id = tokenizer.eos_token_id)
+                                   # length_penalty=length_penalty)
         bot_reply = tokenizer.decode(bot_reply.squeeze()).replace('<|endoftext|>', '')
         bot_reply = textwrap.fill(bot_reply, width=75)
         print(bash_format_text('Aubrey: {}'.format(bot_reply), 'YELLOW', 'BOLD'))
